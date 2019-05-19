e80e401adcbaaade9fadb870911afba406967d92
Antonio <hadrovic1@gmail.com>
2019-05-19 21:02:07 +0200

    Sunday evening commt

--- script.js
+++ script.js
@@ -1,114 +1,57 @@
+// const catApiURL = 'https://api.thecatapi.com/v1/images/search'; // cat Api endpoint;
 
-
-const catApiURL = 'https://api.thecatapi.com/v1/images/search'; // cat Api endpoint;
+const catApiURL = 'https://api.thecatapi.com/v1/images/search?limit=10&order=Desc';
 
 const DOM = {
-	resultsContainer: document.querySelector(".js__search__results"),
-	search__button: document.querySelector(".js__search__button")
+    resultsContainer: document.querySelector(".js__search__results"),
+    search__button: document.querySelector(".js__search__button")
 };
 
+
+
 DOM.search__button.addEventListener("click", e => {
-    fetchData(catApiURL)
+    
+    fetchData(catApiURL);
+    
+    
 });
-
-
-
 
 
 
 
 function fetchData() {
     fetch(catApiURL)
-  .then(response => response.json())
-  .then(data => {
-    const catRandom = document.createElement('div')
-    data.forEach(cat => {
-       catRandom.innerHTML +=
-       `
-       
+        
+        .then(response => response.json())
+        
+        .then(data => {
+            const catRandom = 
+            
+            document.createElement('div')
+
+            catRandom.id = "results"
+
+            
+            
+            data.forEach(cat => {
+                catRandom.innerHTML +=
+                    `
        <div class="div__style">  
          <img class="responsive__img" src=" ${cat.url}">
         </div>
-         
-      
        `
-    })
-// This is where I would like my fetch request to be on the page
-    document.body.append(catRandom)
-  })
-  .catch(function(error) {
-    console.log('There has been a problem with your fetch operation: ', error.message)
-    })
+            })
+            // This is where I would like my fetch request to be on the page
+            
+            document.body.append(catRandom)
+        })
+        .catch(function (error) {
+            console.log('Kratos says - Boy, there is error in your fetch: ', error.message)
+        })
 
 }
 
-//AFTER
-
-// function fetchData() {
-//     fetch(req)
-//     .then( (response)=>{
-//         if(response.ok){
-//             return response.json();
-//         }else{
-//             throw new Error('Kratos says: error');
-//         }
-//     })
-//     .then( (jsonData) =>{
-//         console.log(jsonData);
-//     })
-//     .catch( (err) =>{
-//         console.log('ERROR:', err.message);
-//     });
-// }
 
 
-// const resultsModel = {
-// 	init: function (data) {
-// 		this.status = data.status;
-// 		this.url = data.url;
-// 	}
-// }
-
-// function convert(fetchData) {
-//     const cat = Object.create(resultsModel);
-//     cat.init(fetchData);
-//     generateImg(cat)
-
-// }
-
-// console.log(convert(fetchData))
-
-
-// function fetchData() {
-//     fetch(res)
-//     .then( (response)=>{
-//         if(response.ok){
-//             return response.json();
-//         }else{
-//             throw new Error('Kratos says: error');
-//         }
-//     })
-//     .then( (jsonData) =>{
-//         console.log(jsonData);
-        
-        
-//     })
-//     .catch( (err) =>{
-//         console.log('ERROR:', err.message);
-//     });
-// }
-
-
-// let myHeaders = new Headers({
-//     'Content-Type' : 'application/json',
-//     'x-api-key' : '77393ee4e-fb65-4cb4-80cc-1f086c11a480',
-    
-// });
-
-// let req = new Request(catApiURL, {
-//     method: 'GET',
-//     headers: myHeaders,
-//     mode: 'cors'
-// });
 
 
