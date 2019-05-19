3464078e64f2085f9503d3a8384e9e24dc367a88
Antonio <hadrovic1@gmail.com>
2019-05-19 21:32:32 +0200

    Add jQurey for clearing the DOM prior to click

--- script.js
+++ script.js
@@ -10,10 +10,11 @@ const DOM = {
 
 
 DOM.search__button.addEventListener("click", e => {
-    
+
     fetchData(catApiURL);
-    
-    
+    $( ".results" ).empty()
+
+
 });
 
 
@@ -21,37 +22,32 @@ DOM.search__button.addEventListener("click", e => {
 
 function fetchData() {
     fetch(catApiURL)
-        
+
         .then(response => response.json())
-        
+
         .then(data => {
-            const catRandom = 
-            
-            document.createElement('div')
+            const catRandom =
 
-            catRandom.id = "results"
+                document.createElement('div')
 
+            catRandom.className = "results"
             
-            
+
             data.forEach(cat => {
                 catRandom.innerHTML +=
                     `
-       <div class="div__style">  
-         <img class="responsive__img" src=" ${cat.url}">
-        </div>
-       `
+                    <div class="div__style">  
+                        <img class="responsive__img" src=" ${cat.url}">
+                        </div>
+                    `
             })
             // This is where I would like my fetch request to be on the page
             
             document.body.append(catRandom)
+           
         })
         .catch(function (error) {
             console.log('Kratos says - Boy, there is error in your fetch: ', error.message)
         })
 
 }
-
-
-
-
-
